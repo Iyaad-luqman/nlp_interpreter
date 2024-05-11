3232112b302ab946990339f8c1bf7abfc28d15f1
init_6 <iyaadluq@gmail.com>
2024-05-11 19:16:28 +0530

    v1.0.0 Uploaded to Pypi

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     version='1.0.0',
     description='A package for translating Natural language using NLP processing with Ollama and Gemini into actions.',
     author='Iyaad Luqman K',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Iyaad-luqman/nlp_interpreter',
+    license='MIT',
     author_email='iyaadluq@gmail.com',
     packages=find_packages(),
     install_requires=[
